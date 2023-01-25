17339c62bd3bc6805420a145390bacfea7a8379b
VickArmand <victormaina1962@gmail.com>
2023-01-25 00:21:13 +0000

    resolve

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 class Square:
+    
     """Class Square that defines a square object
+    
     """
     def __init__(self, size):
 
@@ -9,5 +11,6 @@ class Square:
 
         Args:
             param1 (int): size of the square
+        
         """
         self.__size = size
