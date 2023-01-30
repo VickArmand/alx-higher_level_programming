93c657e3f06c274d18b9d690551c0137fadf72ca
VickArmand <victormaina1962@gmail.com>
2023-01-30 14:29:45 +0000

    first commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Created by VickArmand """
+
+class Rectangle:
+      """This class defines a rectangle """
+      pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+Welcome to more adventures of classes
