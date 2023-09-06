6226cf987c053eaea15aad2228231ed8a0a44853
VickArmand <victormaina1962@gmail.com>
2023-09-06 03:13:12 +0300

    5-print_comb2.py

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(100):
+    if (i < 10):
+        print(f"0{i}", end=", ")
+    else:
+        if (i == 99):
+            print(f"{i}")
+        else:
+            print(f"{i}", end=", ")
