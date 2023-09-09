38573dc15393a7d2bcdddd5ae8200dc37fe22756
VickArmand <victormaina1962@gmail.com>
2023-09-09 13:02:25 +0300

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,7 @@
 for i in range(100):
     if (i % 10 <= i / 10):
         continue
+    elif (i / 10 >= 8):
+        print("{:02d}".format(i))
     else:
         print("{:02d}".format(i), end=", ")
