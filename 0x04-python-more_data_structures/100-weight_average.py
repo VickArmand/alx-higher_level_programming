70b5f7dece9949d408466645fb94b022dd852af6
VickArmand <victormaina1962@gmail.com>
2023-09-19 06:05:39 +0300

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    score = 0
+    weight = 0
+    for i in my_list:
+        prod = 1
+        length = len(i)
+        weight += i[length - 1]
+        for j in i:
+            prod *= j
+        score += prod
+    return score / weight
