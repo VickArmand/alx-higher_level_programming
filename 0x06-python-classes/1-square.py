15553810fdfc60427f8b4dad883da3d18f46452b
VickArmand <victormaina1962@gmail.com>
2023-09-27 00:01:03 +0300

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Square that defines a square
+
+    """
+    def __init__(self, size):
+        """
+        __init__ method is run as soon as an object of a class is instantiated
+        Args
+            size (int / float): length of square
+        """
+        self.__size = size
