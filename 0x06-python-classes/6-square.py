b04fc28408db1023a2a95f5775d780b005e23fdf
VickArmand <victormaina1962@gmail.com>
2023-09-27 04:07:03 +0300

    square print pt2

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,12 +50,14 @@ class Square:
         Args
             value (tuple): replacement for position field
         """
-        if (isinstance(value, tuple) and len(value) == 2) and (
-                isinstance(value[0], int) and isinstance(value[1], int)
-                and value[0] >= 0 and value[1] >= 0):
-            self.__position = value
-        else:
+        if (not isinstance(value, tuple) and len(value) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
+        if isinstance(value[0], int) and isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] >= 0 and value[1] >= 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def area(self):
         """
