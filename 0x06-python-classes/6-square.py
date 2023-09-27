ed90282045071832fbcc53e261f44b637a48ce6d
VickArmand <victormaina1962@gmail.com>
2023-09-27 03:52:48 +0300

    square print pt2

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,13 +18,7 @@ class Square:
             position (tuple): a tuple of x and y coordinates
         """
         self.__position = position
-
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     @property
     def size(self):
