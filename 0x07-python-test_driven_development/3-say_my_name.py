671aafa167f29971c502eaac95a151e8d595cee1
VickArmand <victormaina1962@gmail.com>
2023-10-06 14:34:46 +0300

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""
+This module holds say_my_name function
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    say_my_name: function that prints a persons full name
+    Raises
+        TypeError:
+            if first_name must be a string - raised
+            if first_name is not a string
+            last_name must be a string - raised if last_name is not a string
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
