f6a1857136926d9adb1446ffbb48b1d82fb328ac
VickArmand <victormaina1962@gmail.com>
2023-10-13 20:05:02 +0300

    c is fun

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module hosts append_after function
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file,
+    after each line containing a specific string
+    Args
+        filename: name of file
+        search_string: search word
+        new_string: text to be appended
+    """
+    data = []
+    with open(filename, "r") as f:
+        line = f.readline()
+        while line != "":
+            if search_string in line:
+                data.append(line)
+                data.append(new_string)
+            else:
+                data.append(line)
+            line = f.readline()
+    print(data)
+    with open(filename, "w") as f:
+        f.writelines(data)
