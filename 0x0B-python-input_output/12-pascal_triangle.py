613feec678bb36fc65132c004ed64a605655d2c4
VickArmand <victormaina1962@gmail.com>
2023-10-13 16:16:03 +0300

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers
+    representing the Pascal’s triangle of n
+    Args
+        n: integer
+    """
+    new_list = [[1]]
+    if n <= 0:
+        return []
+    for i in range(1, n):
+        length = len(new_list[i - 1])
+        for j in range(length + 1):
+            if j == 0:
+                new_list.append([1])
+            elif j == length:
+                new_list[i].append(1)
+            else:
+                new_list[i].append(
+                        new_list[i - 1][j - 1] + new_list[i - 1][j])
+    return new_list
