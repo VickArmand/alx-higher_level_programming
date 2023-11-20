3002cf455f39c649ed7f13f4ecdfda461094c123
VickArmand <victormaina1962@gmail.com>
2023-11-20 03:47:21 +0300

    corrections

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,4 +3,4 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT ts.title, tsg.genre_id FROM tv_shows ts LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id WHERE tsg.genre_id IS NULL;
+SELECT ts.title, tsg.genre_id FROM tv_shows ts LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id WHERE tsg.genre_id IS NULL ORDER BY ts.title;
