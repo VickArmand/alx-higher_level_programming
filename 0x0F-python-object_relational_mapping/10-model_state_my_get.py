b155744c04a184afc6a73a447530b17fd24af7b7
VickArmand <victormaina1962@gmail.com>
2023-12-18 16:17:48 +0300

    corrections v6

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
-    records = session.query(State).filter(State.name == sys.argv[4])
-    try:
+    records = session.query(State).filter(State.name == sys.argv[4]).all()
+    if len(records) == 0:
+        print("Not found")
+    else:
         for row in records:
             print("{}".format(row.id))
-    except:
-        print("Not found")
