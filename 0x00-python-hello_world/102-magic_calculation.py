d4679b6523be493021b91176e2137376f4620e1a
VickArmand <victormaina1962@gmail.com>
2024-01-02 12:26:27 +0300

    bytecode v1

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+"""
+This module hosts the magic_calculation function
+that implements the following bytecode
+3       0 LOAD_CONST    1 (98)
+        3 LOAD_FAST     0 (a)
+        6 LOAD_FAST     1 (b)
+        9 BINARY_POWER
+        10 BINARY_ADD
+        11 RETURN VALUE
+"""
 
+
+def magic_calculation(a, b):
+    """"""
+    return 98 + a ** b
