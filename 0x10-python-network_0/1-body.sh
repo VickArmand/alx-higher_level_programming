55607e670008d20bc48a1e620f59f71e78474b0b
VickArmand <victormaina1962@gmail.com>
2024-01-07 17:09:06 +0300

    corrections v1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a GET request to it and displays the response
-# Displays onlybody of a 200 status code
+# Bash script that takes in a URL, sends a GET request to it and displays the response Displays onlybody of a 200 status code
 curl -sL "$1"
