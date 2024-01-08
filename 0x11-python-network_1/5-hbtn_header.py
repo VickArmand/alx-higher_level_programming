64c359b7ad7dcf3cccce7949f1212c97b06d471d
VickArmand <victormaina1962@gmail.com>
2024-01-08 05:40:51 +0300

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,6 +4,8 @@ Python script that takes in a URL, sends a request to the URL and
 displays the value of the X-Request-Id variable found in the header
 of the response.
 You must use the packages requests and sys only
+You must use get to access to dictionary value by key
+(it won’t throw an exception if the key doesn’t exist in the dictionary)
 """
 import sys
 import requests
@@ -11,4 +13,4 @@ import requests
 
 if __name__ == "__main__":
     req = requests.get(sys.argv[1])
-    print(req.headers['X-Request-Id'])
+    print(req.headers.get('X-Request-Id'))
