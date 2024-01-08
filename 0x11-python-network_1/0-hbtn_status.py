535b9825cfad3e7120c49940e31e3bc76481e20c
VickArmand <victormaina1962@gmail.com>
2024-01-08 04:07:00 +0300

    make the code executable when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,11 @@
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    page = response.read()
-print("Body response:")
-print("\t- type: {}".format(type(page)))
-print("\t- content: {}".format(page))
-print("\t- utf8 content: {}".format(page.decode("utf-8")))
+if __name__ == "__main__":
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(page)))
+    print("\t- content: {}".format(page))
+    print("\t- utf8 content: {}".format(page.decode("utf-8")))
