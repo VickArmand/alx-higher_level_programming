3b9c6eb63ac5de65ade3cbde69afb4036bfd3c5b
VickArmand <victormaina1962@gmail.com>
2024-01-31 14:54:06 +0300

    corrections

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,6 +7,6 @@
 const req = require('request');
 req(process.argv[2], (error, response, body) => {
   if (error) console.log(error);
-  console.log(response.statusCode);
+  console.log("code: "+response.statusCode);
 }
 );
